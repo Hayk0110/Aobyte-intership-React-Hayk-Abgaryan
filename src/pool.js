b8e1d5127b886f3bcbd9e0d9754bdd234cbdfde5
Hayk Abgaryan <hayk.abgaryan.0110@gmail.com>
2023-07-16 23:44:06 +0400

    fixed pool.js

--- src/pool.js
+++ src/pool.js
@@ -4,15 +4,18 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "ornare quam viverra orci sagittis eu",
                 rate: 6
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "netus et malesuada fames ac turpis",
                 rate: 7
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "facilisis mauris sit amet massa",
                 rate: 1
             }
         ]
@@ -22,23 +25,28 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "ultrices eros in cursus turpis massa",
                 rate: 4
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "etiam tempor orci eu lobortis elementum",
                 rate: 5
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "at auctor urna nunc id cursus",
                 rate: 2
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 4,
+                text: "sit amet purus gravida quis blandit",
                 rate: 1
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 5,
+                text: "nulla at volutpat diam ut venenatis",
                 rate: 9
             
         }]
@@ -48,15 +56,18 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "eleifend mi in nulla posuere sollicitudind",
                 rate: 6
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "dignissim cras tincidunt lobortis feugiat vivamus",
                 rate: 4
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "amet nulla facilisi morbi tempus iaculis",
                 rate: 9
             }
         ]
@@ -66,15 +77,18 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "cursus sit amet dictum sit amet",
                 rate: 2
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "amet nulla facilisi morbi tempus iaculis",
                 rate: 8
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "mattis rhoncus urna neque viverra justo",
                 rate: 10
             }
         ]
@@ -84,23 +98,28 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
+                id: 1,
                 text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
                 rate: 1
             },
             {
+                id: 2,
                 text: "et malesuada fames ac turpis egestas",
                 rate: 3
             },
             {
+                id: 3,
                 text: "arcu dui vivamus arcu felis bibendum",
                 rate: 4
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 4,
+                text: "gravida arcu ac tortor dignissim convallis",
                 rate: 6
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 5,
+                text: "consectetur purus ut faucibus pulvinar elementum",
                 rate: 5
             },
         ]
@@ -110,27 +129,33 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
+                id: 1,
                 text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
                 rate: 6
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "ornare quam viverra orci sagittis eu",
                 rate: 6
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "bibendum enim facilisis gravida neque convallis",
                 rate: 5
             },
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 4,
+                text: "molestie nunc non blandit massa enim",
                 rate: 4
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 5,
+                text: "eget est lorem ipsum dolor sit",
                 rate: 10
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 6,
+                text: "ac turpis egestas sed tempus urna",
                 rate: 5
             }
         ]
@@ -140,15 +165,18 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "sit amet nisl suscipit adipiscing bibendum",
                 rate: 4
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "a diam maecenas sed enim ut sem",
                 rate: 2
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "nulla aliquet enim tortor at auctor urna",
                 rate: 5
             }
         ]
@@ -158,23 +186,28 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "ut aliquam purus sit amet luctus venenatis",
                 rate: 2
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "nunc mattis enim ut tellus elementum sagittis",
                 rate: 1
             },
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 3,
+                text: "vitae tempus quam pellentesque nec nam aliquam",
                 rate: 9
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 4,
+                text: "ullamcorper dignissim cras tincidunt lobortis feugiat vivamuss",
                 rate: 7
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 5,
+                text: "gravida in fermentum et sollicitudin ac orci",
                 rate: 8
             }
         ]
@@ -184,15 +217,18 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "molestie ac feugiat sed lectus vestibulum mattis",
                 rate: 7
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 2,
+                text: "dictum sit amet justo donec enim diam",
                 rate: 8
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 3,
+                text: "elementum nisi quis eleifend quam adipiscing vitae",
                 rate: 9
             }
         ]
@@ -202,19 +238,23 @@ export const posts = [
         postText: "Lorem ispum nibh tortor id aliquet lectus proin nibh nisl condimentum",
         comments:[
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 1,
+                text: "et netus et malesuada fames ac turpis",
                 rate: 7
             },
             {
-                text: "mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod",
+                id: 2,
+                text: "neque volutpat ac tincidunt vitae semper quis",
                 rate: 10
             },
             {
-                text: "et malesuada fames ac turpis egestas",
+                id: 3,
+                text: "ornare quam viverra orci sagittis eu",
                 rate: 8
             },
             {
-                text: "arcu dui vivamus arcu felis bibendum",
+                id: 4,
+                text: "sit amet purus gravida quis blandit",
                 rate: 9
             }
         ]
